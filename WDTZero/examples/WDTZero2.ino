7ff0f8062fa78dbe1f993a94d6cd4fa5e5edfda6
John V <46841841+javos65@users.noreply.github.com>
2019-01-23 16:04:02 +0100

    V1.2.0 update

---
+++ WDTZero/examples/WDTZero2.ino
@@ -0,0 +1,37 @@
+
+#include <WDTZero.h>
+
+byte wdtCounter, wdtCounterReset;
+WDTZero MyWatchDoggy; // Define WDT with hard-Watchdog @ 4 seconds - hardstop when processor is stalled. Configure your soft-counter at .Setup(). 
+
+void setup() {
+  int t = 20; //Initialize serial and wait for port to open, max 10 seconds
+  Serial.begin(9600);
+  while (!Serial) {
+    delay(500);
+    if ( (t--) == 0 ) break;
+  }
+ Serial.print("\nWDTZero-Demo : Setup Soft Watchdog at 32S interval"); 
+ MyWatchDoggy.setup(WDT_SOFTCYCLE32S);  // initialize WDT-softcounter refesh cycle on 16sec interval
+}
+
+void loop() {
+  unsigned int t = 0;
+
+
+  Serial.print("\nWatchdog Test1 - run 60 seconds with a WDT.clear()\n");
+  for (t = 1; t < 60; ++t) {
+    MyWatchDoggy.clear();  // refresh wdt - before it loops
+    delay(950);
+    Serial.print(t);Serial.print(".");
+  }
+
+  Serial.print("\n\nWatchdog Test2 - Free run wait for reset @32s\n");
+  for (t = 1; t > 0; ++t) {
+    delay(950);
+    Serial.print(t);Serial.print(".");
+    }
+    
+Serial.print("\nYou will never read this line");
+
+}
